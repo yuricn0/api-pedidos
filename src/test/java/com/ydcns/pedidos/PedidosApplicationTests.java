77717b1323bfe526acf5597c18656ee2c94a2d6b
Yuri Nunes <yuri.cruz10@hotmail.com>
2025-03-15 17:50:13 -0300

    Iniciando aplicação

---
+++ src/test/java/com/ydcns/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydcns.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
