53975aa1440e9341e6bb6ead8a21ffdc51dae26f
Yuri Nunes <yuri.cruz10@hotmail.com>
2025-03-17 15:22:17 -0300

    OrderStatus enum add

--- src/main/java/com/ydcns/pedidos/config/TestConfig.java
+++ src/main/java/com/ydcns/pedidos/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.ydcns.pedidos.entities.Order;
 import com.ydcns.pedidos.entities.User;
+import com.ydcns.pedidos.entities.enums.OrderStatus;
 import com.ydcns.pedidos.repositories.OrderRepository;
 import com.ydcns.pedidos.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {   // implementar commandL
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "95545588", "18765656");
 		
-		Order o1 = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));	
